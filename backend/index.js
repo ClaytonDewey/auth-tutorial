ad73163080babddf3d9ac828ba52455b564b7eb3
ClaytonDewey <clay@dryadmedia.com>
2025-07-28 14:48:30 -0500

    feat: Import and use cookieParser

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import { connectDB } from './db/connectDB.js';
 import authRoutes from './routes/auth.route.js';
 
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows to parse incoming requests :req.body
+app.use(cookieParser()); // allows to parse cookies
 
 app.use('/api/auth', authRoutes);
 
