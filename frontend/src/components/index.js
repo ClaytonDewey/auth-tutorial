709eb7bc59ff8a1047fea962e9acca9f0e44c28a
Clayton Dewey <clay@dryadmedia.com>
2025-07-28 21:31:39 -0500

    create: Set up all pages

---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+export { FloatingShape } from './FloatingShape';
+export { Input } from './Input';
+export { LoadingSpinner } from './LoadingSpinner';
+export { PasswordStrengthMeter } from './PasswordStrengthMeter';
