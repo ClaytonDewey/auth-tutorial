fe94867e2f27c99c977b5da9b5dd8bdbb5ce3add
Clayton Dewey <clay@dryadmedia.com>
2025-07-29 13:28:11 -0500

    feat: Install cors

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { connectDB } from './db/connectDB.js';
 import authRoutes from './routes/auth.route.js';
@@ -9,6 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(express.json()); // allows to parse incoming requests :req.body
 app.use(cookieParser()); // allows to parse cookies
 
