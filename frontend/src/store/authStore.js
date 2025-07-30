89a93273edef68c9f34a8ca2c86bb8270b6558a5
Clayton Dewey <clay@dryadmedia.com>
2025-07-29 21:18:31 -0500

    feat: Add forgotPassword method

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -70,7 +70,6 @@ export const useAuthStore = create((set) => ({
       set({ error: 'Error logging out', isLoading: false });
     }
   },
-
   verifyEmail: async (code) => {
     set({ isLoading: true, error: null });
     try {
@@ -89,7 +88,6 @@ export const useAuthStore = create((set) => ({
       throw error;
     }
   },
-
   checkAuth: async () => {
     set({ isCheckingAuth: true, error: null });
     try {
@@ -101,6 +99,22 @@ export const useAuthStore = create((set) => ({
       });
     } catch (error) {
       set({ error: null, isCheckingAuth: false, isAuthenticated: false });
+    }
+  },
+  forgotPassword: async (email) => {
+    set({ isLoading: true, error: null, message: null });
+    try {
+      const response = await axios.post(`${API_URL}/forgot-password`, {
+        email,
+      });
+      set({ message: response.data.message, isLoading: false });
+    } catch (error) {
+      set({
+        isLoading: false,
+        error:
+          error.response.data.message || 'Error sending reset password email',
+      });
+      throw error;
     }
   },
 }));
