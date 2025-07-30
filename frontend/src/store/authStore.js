10c40e697f659bf172acc560438885d34bfa6801
Clayton Dewey <clay@dryadmedia.com>
2025-07-29 20:42:56 -0500

    feat: Add logout method

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -56,6 +56,21 @@ export const useAuthStore = create((set) => ({
     }
   },
 
+  logout: async () => {
+    set({ isLoading: true, error: null });
+    try {
+      await axios.post(`${API_URL}/logout`);
+      set({
+        user: null,
+        isAuthenticated: false,
+        error: null,
+        isLoading: false,
+      });
+    } catch (error) {
+      set({ error: 'Error logging out', isLoading: false });
+    }
+  },
+
   verifyEmail: async (code) => {
     set({ isLoading: true, error: null });
     try {
