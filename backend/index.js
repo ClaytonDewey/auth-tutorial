a5d1f64b10a5d0ca1f5097f3f0af83de0f0d2925
ClaytonDewey <clay@dryadmedia.com>
2025-07-31 11:35:27 -0500

    refactor: Update for deployment

--- backend/index.js
+++ backend/index.js
@@ -2,21 +2,34 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+
 import { connectDB } from './db/connectDB.js';
+
 import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
-app.use(express.json()); // allows to parse incoming requests :req.body
-app.use(cookieParser()); // allows to parse cookies
+
+app.use(express.json()); // allows us to parse incoming requests:req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
 
 app.use('/api/auth', authRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('/{*any}', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
 app.listen(PORT, () => {
   connectDB();
-  console.log(`Server is running on port ${PORT}`);
+  console.log('Server is running on port: ', PORT);
 });
